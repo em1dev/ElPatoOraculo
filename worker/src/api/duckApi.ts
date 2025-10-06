4926c3a531ef28bff0817fd776ff46544ab08fc2
EmyDev <115660028+em1dev@users.noreply.github.com>
2025-10-06 12:18:36 +0100

    fix duck api sending broken images

--- worker/src/api/duckApi.ts
+++ worker/src/api/duckApi.ts
@@ -1,9 +1,47 @@
 import { config } from "../config";
 
-const duckErrorCodeUrl = (code: number) => `https://random-d.uk/api/http/${code}.jpg`;
+const API_URL = 'https://random-d.uk/api';
+const duckErrorCodeUrl = (code: number) => `${API_URL}/http/${code}.jpg`;
+
+interface DuckList 
+{
+  gif_count: number,
+  gifs: Array<string>,
+  http: Array<string>,
+  image_count: number,
+  images: Array<string>,
+}
+
+const getRandomDuckUrlFromList = async () => {
+  try {
+    const listResp = await fetch(`${API_URL}/list`);
+    const data = await listResp.json() as DuckList;
+    // for some reason images ending with capital JPG are not working on this api :v
+    //    probably due to some internal issue, for now ill exclude them to avoid broken images
+    let imgs = [
+      ...data.images.filter(i => !i.endsWith('JPG')),
+      ...data.gifs
+    ];
+
+    // exclude banned ducks
+    imgs = imgs
+    .filter(i => 
+      !config.bannedDuckIds.includes(i.split('.').at(0) ?? "")
+    );
+
+    const randomImg = imgs[Math.floor(Math.random() * imgs.length)];
+    return `${API_URL}/${randomImg}`;
+  } catch (e) {
+    return;
+  }
+}
 
 const getRandomDuckUrl = async ():Promise<string> => {
   try {
+    // try workaround first if that fails fall back to previous implementation since this api is quite unreliable (prob better to just store the images)
+    const result = await getRandomDuckUrlFromList();
+    if (result) return result;
+
     const resp = await fetch('https://random-d.uk/api/random');
     const { url } = await resp.json() as { url: string };
 
